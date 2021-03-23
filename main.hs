86c76df027886a99711780db93c3d41b880634e6
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2021-03-23 00:47:35 +0000

    Write last function

--- main.hs
+++ main.hs
@@ -11,3 +11,22 @@ signum n
         | n < 0     = -1
         | n == 0    = 0
         | otherwise = 1
+
+(&&) :: Bool -> Bool -> Bool
+True && True = True
+_    && _    = False
+
+(||?) :: Bool -> Bool -> Bool
+True ||? b = b
+False ||? _ = False
+
+--and' :: Bool -> Bool -> Bool
+--True `and'` True = True
+--_    `and'` _    = False
+
+last' :: [Int] -> Int
+last' [x] = x
+last' (_:xs) = last' xs
+
+last'' :: [Int] -> Int
+last'' xs = head (reverse xs)
